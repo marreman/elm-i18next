024d8d41e1e9cc039782656514dbbcf8628720ef
Martin Liberg <martin.liberg@m.co>
2021-02-23 21:12:03 +0100

    Improve tests
    * Break up test suite a bit so that it's easier to understand how things
      work.
    * Validate all Elm output by trying to parse it and fail the tests if it fails.
    * Use fuzzers to cover more cases.

--- tests/ElmTests.elm
+++ tests/ElmTests.elm
@@ -2,113 +2,172 @@ module ElmTests exposing (..)
 
 import Dict
 import Elm
-import Expect
-import Test exposing (Test, test)
-import Text
+import Elm.DSLParser
+import Expect exposing (Expectation)
+import Helper exposing (..)
+import Test exposing (..)
+import Text exposing (Text(..))
 
 
-suite : Test
-suite =
-    test "Text.fromJson" <|
-        \_ ->
-            Dict.fromList
-                [ ( []
-                  , Dict.fromList
-                        [ ( "foo", [ Text.Static "bar" ] )
-                        , ( "1bad__key", [ Text.Static "with", Text.Parameter "1bad_param" ] )
-                        , ( "repeated_param", [ Text.Parameter "foo", Text.Static ", I repeat, ", Text.Parameter "foo" ] )
-                        ]
-                  )
-                , ( [ "temporality", "date_formats" ]
-                  , Dict.fromList
-                        [ ( "year difference"
-                          , [ Text.Static "The difference between "
-                            , Text.Parameter "first_year"
-                            , Text.Static " and "
-                            , Text.Parameter "second_year"
-                            , Text.Static " is "
-                            , Text.Parameter "year_difference"
-                            , Text.Static "."
-                            ]
-                          )
-                        ]
-                  )
-                , ( [ "temporality" ]
-                  , Dict.fromList
-                        [ ( "current_date_and_time"
-                          , [ Text.Static "The date is "
-                            , Text.Parameter "date"
-                            , Text.Static " and the time is "
-                            , Text.Parameter "time"
-                            ]
-                          )
-                        , ( "current_time"
-                          , [ Text.Static "The time is "
-                            , Text.Parameter "time"
-                            , Text.Static " now."
-                            ]
-                          )
-                        ]
-                  )
-                ]
-                |> Elm.fromText "Text"
-                |> Expect.equal
-                    [ { content = module1, path = [ "Text", "Temporality", "DateFormats" ] }
-                    , { content = module2, path = [ "Text", "Temporality" ] }
-                    , { content = module3, path = [ "Text" ] }
+tests : Test
+tests =
+    describe "Elm.fromText"
+        [ test "it works with the most simple static case" <|
+            \_ ->
+                singleTextModule "bar"
+                    [ Static "baz"
                     ]
+                    |> Elm.fromText "foo"
+                    |> expectEqualAndValidFiles [ simpleStaticFile ]
+        , test "it works with the most simple parameterized case" <|
+            \_ ->
+                singleTextModule "bar"
+                    [ Static "baz"
+                    , Parameter "bonk"
+                    ]
+                    |> Elm.fromText "foo"
+                    |> expectEqualAndValidFiles [ simpleParameterizedFile ]
+        , test "it camel cases everything" <|
+            \_ ->
+                singleTextModule "foo-19(}_bar-BAZ__@#$%^&"
+                    [ Static "im left !@#$%^&*()_+ alone"
+                    , Parameter "te-23332st$%^&ing1"
+                    ]
+                    |> Elm.fromText "a very-^&*%*!STRANGE---ååname"
+                    |> expectEqualAndValidFiles [ camelCasedFile ]
+        , test "it prefixes bad names" <|
+            \_ ->
+                singleTextModule "456"
+                    [ Static "im left alone"
+                    , Parameter "789"
+                    ]
+                    |> Elm.fromText "123"
+                    |> expectEqualAndValidFiles [ prefixedFile ]
+        , test "it returns multiple files" <|
+            \_ ->
+                Dict.fromList
+                    [ ( [], Dict.fromList [ ( "a", [ Static "a" ] ) ] )
+                    , ( [ "b" ], Dict.fromList [ ( "b", [ Static "b" ] ) ] )
+                    , ( [ "b", "c" ], Dict.fromList [ ( "c", [ Static "c" ] ) ] )
+                    , ( [ "b", "c", "d" ], Dict.fromList [ ( "d", [ Static "d" ] ) ] )
+                    ]
+                    |> Elm.fromText "a"
+                    |> expectEqualAndValidFiles multipleFiles
+        ]
 
 
-module1 : String
-module1 =
-    """module Text.Temporality.DateFormats exposing (..)
+
+-- EXPECTED FILES
 
 
-yearDifference : (String -> a) -> { firstYear : a, secondYear : a, yearDifference : a } -> List a
-yearDifference fromString parameters =
-    [ fromString "The difference between "
-    , parameters.firstYear
-    , fromString " and "
-    , parameters.secondYear
-    , fromString " is "
-    , parameters.yearDifference
-    , fromString "."
+simpleStaticFile : Elm.File
+simpleStaticFile =
+    { path = [ "Foo" ]
+    , content =
+        """module Foo exposing (..)
+
+
+bar : String
+bar =
+    "baz"
+"""
+    }
+
+
+simpleParameterizedFile : Elm.File
+simpleParameterizedFile =
+    { path = [ "Foo" ]
+    , content =
+        """module Foo exposing (..)
+
+
+bar : (String -> a) -> { bonk : a } -> List a
+bar fromString parameters =
+    [ fromString "baz", parameters.bonk ]
+"""
+    }
+
+
+camelCasedFile : Elm.File
+camelCasedFile =
+    { path = [ "AVeryStrangeName" ]
+    , content =
+        """module AVeryStrangeName exposing (..)
+
+
+foo19BarBaz : (String -> a) -> { te23332stIng1 : a } -> List a
+foo19BarBaz fromString parameters =
+    [ fromString "im left !@#$%^&*()_+ alone", parameters.te23332stIng1 ]
+"""
+    }
+
+
+prefixedFile : Elm.File
+prefixedFile =
+    { path = [ "123" ]
+    , content =
+        """module 123 exposing (..)
+
+
+t456 : (String -> a) -> { p789 : a } -> List a
+t456 fromString parameters =
+    [ fromString "im left alone", parameters.p789 ]
+"""
+    }
+
+
+multipleFiles : List Elm.File
+multipleFiles =
+    [ { content = "module A.B.C.D exposing (..)\n\n\nd : String\nd =\n    \"d\"\n"
+      , path = [ "A", "B", "C", "D" ]
+      }
+    , { content = "module A.B.C exposing (..)\n\n\nc : String\nc =\n    \"c\"\n"
+      , path = [ "A", "B", "C" ]
+      }
+    , { content = "module A.B exposing (..)\n\n\nb : String\nb =\n    \"b\"\n"
+      , path = [ "A", "B" ]
+      }
+    , { content = "module A exposing (..)\n\n\na : String\na =\n    \"a\"\n"
+      , path = [ "A" ]
+      }
     ]
-"""
 
 
-module2 : String
-module2 =
-    """module Text.Temporality exposing (..)
+
+-- HELPERS
 
 
-currentDateAndTime : (String -> a) -> { date : a, time : a } -> List a
-currentDateAndTime fromString parameters =
-    [ fromString "The date is ", parameters.date, fromString " and the time is ", parameters.time ]
+expectEqualAndValidFiles : List Elm.File -> List Elm.File -> Expectation
+expectEqualAndValidFiles expectedFiles actualFiles =
+    Expect.all
+        [ Expect.equalLists expectedFiles
+        , expectValidFiles
+        ]
+        actualFiles
 
 
-currentTime : (String -> a) -> { time : a } -> List a
-currentTime fromString parameters =
-    [ fromString "The time is ", parameters.time, fromString " now." ]
-"""
+expectValidFiles : List Elm.File -> Expectation
+expectValidFiles files =
+    let
+        validateFile file =
+            Result.andThen <|
+                \_ ->
+                    Elm.DSLParser.parse file.content
+                        |> Result.map (\_ -> ())
+                        |> Result.mapError (Tuple.pair file)
+    in
+    case List.foldl validateFile (Ok ()) files of
+        Ok _ ->
+            Expect.pass
+
+        Err ( file, error ) ->
+            Expect.fail <|
+                "I tried to parse this Elm file:\n\n"
+                    ++ indent file.content
+                    ++ "\n\nBut I encountered the following error(s):\n\n"
+                    ++ indent (Debug.toString error)
 
 
-module3 : String
-module3 =
-    """module Text exposing (..)
-
-
-t1badKey : (String -> a) -> { p1badParam : a } -> List a
-t1badKey fromString parameters =
-    [ fromString "with", parameters.p1badParam ]
-
-
-foo : String
-foo =
-    "bar"
-
-
-repeatedParam : (String -> a) -> { foo : a } -> List a
-repeatedParam fromString parameters =
-    [ parameters.foo, fromString ", I repeat, ", parameters.foo ]
-"""
+indent : String -> String
+indent =
+    String.lines >> List.map ((++) "\t") >> String.join "\n"
--- tests/TextTests.elm
+++ tests/TextTests.elm
@@ -2,99 +2,90 @@ module TextTests exposing (..)
 
 import Dict
 import Expect
+import Fuzz
+import Helper exposing (..)
+import Html exposing (text)
 import Json.Encode as E
-import Test exposing (Test, test)
-import Text
+import Test exposing (Test, describe, fuzz, fuzz2, test)
+import Text exposing (..)
 
 
-multiLevel : Test
-multiLevel =
-    test "Multi level" <|
-        \_ ->
-            E.object
-                [ ( "a", E.string "a" )
-                , ( "b"
-                  , E.object
-                        [ ( "b", E.string "b" )
-                        , ( "c"
-                          , E.object
-                                [ ( "c", E.string "c" )
-                                , ( "d"
-                                  , E.object
-                                        [ ( "d", E.string "d" ) ]
-                                  )
-                                ]
-                          )
-                        ]
-                  )
-                ]
-                |> Text.fromJson
-                |> Expect.equal
-                    (Dict.fromList
-                        [ ( [], Dict.fromList [ ( "a", [ Text.Static "a" ] ) ] )
-                        , ( [ "b" ], Dict.fromList [ ( "b", [ Text.Static "b" ] ) ] )
-                        , ( [ "b", "c" ], Dict.fromList [ ( "c", [ Text.Static "c" ] ) ] )
-                        , ( [ "b", "c", "d" ], Dict.fromList [ ( "d", [ Text.Static "d" ] ) ] )
-                        ]
-                    )
-
-
-complex : Test
-complex =
-    test "Text.fromJson" <|
-        \_ ->
-            E.object
-                [ ( "foo", E.string "bar" )
-                , ( "temporality"
-                  , E.object
-                        [ ( "current_time", E.string "The time is {{ time }} now." )
-                        , ( "current_date_and_time", E.string "The date is {{ date }} and the time is {{ time }}" )
-                        , ( "date_formats"
-                          , E.object
-                                [ ( "year difference", E.string "The difference between {{ first_year }} and {{ second_year }} is {{ year_difference }}." )
-                                ]
-                          )
-                        ]
-                  )
-                ]
-                |> Text.fromJson
-                |> Expect.equal
-                    (Dict.fromList
-                        [ ( []
-                          , Dict.fromList
-                                [ ( "foo", [ Text.Static "bar" ] )
-                                ]
-                          )
-                        , ( [ "temporality", "date_formats" ]
-                          , Dict.fromList
-                                [ ( "year difference"
-                                  , [ Text.Static "The difference between "
-                                    , Text.Parameter "first_year"
-                                    , Text.Static " and "
-                                    , Text.Parameter "second_year"
-                                    , Text.Static " is "
-                                    , Text.Parameter "year_difference"
-                                    , Text.Static "."
+tests : Test
+tests =
+    describe "Text.fromJson"
+        [ fuzz2 Fuzz.string Helper.textFuzzer "works with any non empty valid string" <|
+            \name text ->
+                E.object [ ( name, E.string text ) ]
+                    |> Text.fromJson
+                    |> Expect.equal
+                        (singleTextModule name
+                            [ Static text
+                            ]
+                        )
+        , test "an empty string renders an empty list" <|
+            \_ ->
+                E.object [ ( "foo", E.string "" ) ]
+                    |> Text.fromJson
+                    |> Expect.equal
+                        (singleTextModule "foo" [])
+        , fuzz textFuzzer "it constructs one parameter" <|
+            \string ->
+                E.object [ ( "", E.string ("{{" ++ string ++ "}}") ) ]
+                    |> Text.fromJson
+                    |> Expect.equal
+                        (singleTextModule ""
+                            [ Parameter string
+                            ]
+                        )
+        , fuzz2 textFuzzer textFuzzer "it constructs many parameters" <|
+            \param1 param2 ->
+                E.object [ ( "", E.string ("{{" ++ param1 ++ "}}{{" ++ param2 ++ "}}") ) ]
+                    |> Text.fromJson
+                    |> Expect.equal
+                        (singleTextModule ""
+                            [ Parameter param1
+                            , Parameter param2
+                            ]
+                        )
+        , fuzz2 textFuzzer textFuzzer "it preserves static text before, between and around parameters" <|
+            \param1 param2 ->
+                E.object [ ( "", E.string ("one, {{" ++ param1 ++ "}}, three, {{" ++ param2 ++ "}}, five") ) ]
+                    |> Text.fromJson
+                    |> Expect.equal
+                        (singleTextModule ""
+                            [ Static "one, "
+                            , Parameter param1
+                            , Static ", three, "
+                            , Parameter param2
+                            , Static ", five"
+                            ]
+                        )
+        , test "it flattens deeply nested json properly" <|
+            \_ ->
+                E.object
+                    [ ( "a", E.string "a" )
+                    , ( "b"
+                      , E.object
+                            [ ( "b", E.string "b" )
+                            , ( "c"
+                              , E.object
+                                    [ ( "c", E.string "c" )
+                                    , ( "d"
+                                      , E.object
+                                            [ ( "d", E.string "d" ) ]
+                                      )
                                     ]
-                                  )
-                                ]
-                          )
-                        , ( [ "temporality" ]
-                          , Dict.fromList
-                                [ ( "current_date_and_time"
-                                  , [ Text.Static "The date is "
-                                    , Text.Parameter "date"
-                                    , Text.Static " and the time is "
-                                    , Text.Parameter "time"
-                                    ]
-                                  )
-                                , ( "current_time"
-                                  , [ Text.Static "The time is "
-                                    , Text.Parameter "time"
-                                    , Text.Static " now."
-                                    ]
-                                  )
-                                ]
-                          )
-                        ]
-                    )
+                              )
+                            ]
+                      )
+                    ]
+                    |> Text.fromJson
+                    |> Expect.equal
+                        (Dict.fromList
+                            [ ( [], Dict.fromList [ ( "a", [ Static "a" ] ) ] )
+                            , ( [ "b" ], Dict.fromList [ ( "b", [ Static "b" ] ) ] )
+                            , ( [ "b", "c" ], Dict.fromList [ ( "c", [ Static "c" ] ) ] )
+                            , ( [ "b", "c", "d" ], Dict.fromList [ ( "d", [ Static "d" ] ) ] )
+                            ]
+                        )
+        ]
