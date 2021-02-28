ddf6505dc350e5dfe50eeaec14e3d2eb92b3d18d
Martin Liberg <martin.liberg@m.co>
2021-02-28 22:34:41 +0100

    Rename and restructure a bit

--- src/Text.elm
+++ src/Text.elm
@@ -2,7 +2,7 @@ module Text exposing (..)
 
 import Dict exposing (Dict)
 import Json.Decode as Decode exposing (Decoder)
-import Parser exposing ((|.), (|=))
+import Parser exposing ((|.), (|=), Parser)
 
 
 type Text
@@ -25,23 +25,44 @@ type Node
 
 fromJson : Decode.Value -> Result Decode.Error (Dict Path Group)
 fromJson json =
-    Decode.decodeValue parser json
+    Decode.decodeValue decoder json
         |> Result.map
             (\nodes ->
                 flatten [] nodes Dict.empty
-                    |> Dict.foldl
-                        (\path group groups -> Dict.insert (List.reverse path) group groups)
-                        Dict.empty
+                    |> reverseGroupPaths
             )
 
 
-parser : Decoder (Dict String Node)
-parser =
+decoder : Decoder (Dict String Node)
+decoder =
     Decode.dict <|
         Decode.oneOf
-            [ Decode.map (parseText >> Leaf) Decode.string
-            , Decode.map Branch (Decode.lazy (\_ -> parser))
+            [ Decode.map (Parser.run parser >> Result.withDefault [] >> Leaf) Decode.string
+            , Decode.map Branch (Decode.lazy (\_ -> decoder))
             ]
+
+
+parser : Parser (List Text)
+parser =
+    let
+        step parts =
+            Parser.oneOf
+                [ Parser.end |> Parser.map (\_ -> Parser.Done (List.reverse parts))
+                , Parser.oneOf [ parameter, static ] |> Parser.map (\part -> Parser.Loop (part :: parts))
+                ]
+
+        static =
+            Parser.chompUntilEndOr "{{"
+                |> Parser.getChompedString
+                |> Parser.map Static
+
+        parameter =
+            Parser.succeed (String.trim >> Parameter)
+                |. Parser.symbol "{{"
+                |= (Parser.getChompedString <| Parser.chompUntil "}}")
+                |. Parser.symbol "}}"
+    in
+    Parser.loop [] step
 
 
 flatten : Path -> Dict String Node -> Dict Path Group -> Dict Path Group
@@ -67,29 +88,6 @@ flatten path nodes initialGroups =
     Dict.insert path newGroup (Dict.union initialGroups moreGroups)
 
 
-parseText : String -> List Text
-parseText string =
-    let
-        step parts =
-            Parser.oneOf
-                [ Parser.end |> Parser.map (\_ -> Parser.Done (List.reverse parts))
-                , Parser.oneOf [ parameter, static ] |> Parser.map (\part -> Parser.Loop (part :: parts))
-                ]
-
-        static =
-            Parser.chompUntilEndOr "{{"
-                |> Parser.getChompedString
-                |> Parser.map Static
-
-        parameter =
-            Parser.succeed (String.trim >> Parameter)
-                |. Parser.symbol "{{"
-                |= (Parser.getChompedString <| Parser.chompUntil "}}")
-                |. Parser.symbol "}}"
-    in
-    Parser.run (Parser.loop [] step) string
-        |> Result.withDefault []
-
-
-
--- reversePaths : Dict Path Group
+reverseGroupPaths : Dict Path Group -> Dict Path Group
+reverseGroupPaths =
+    Dict.foldl (\path -> Dict.insert (List.reverse path)) Dict.empty
